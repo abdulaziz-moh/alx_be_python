3b8942b8320a9015cb296ce2cb2692fbea4b8c7a
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-06-01 17:18:38 +0300

    try to solve the problem

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,37 +8,28 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 # Use match-case to handle different priority levels
 match priority:
     case "high":
-        message = f"Reminder: '{task}' is a high priority task."
+        print(f"Reminder: '{task}' is a high priority task.", end = "" )
         
         if time_bound == "yes":
-            message += " that requires immediate attention today!"
+            print(" that requires immediate attention today!")
         else:
-            message += ". Consider completing it when you have free time."
+            print(" Consider completing it when you have free time.")
 
 
     case "medium":
-        message = f"Reminder: '{task}' is a medium priority task."
+        print(f"Reminder: '{task}' is a medium priority task.", end = "" )
         if time_bound == "yes":
-            message += " that requires immediate attention today!"
+            print(" that requires immediate attention today!")
         else:
-            message += ". Consider completing it when you have free time."
+            print(" Consider completing it when you have free time.")
 
     case "low":
-        message = f"Note: '{task}' is a low priority task."
+        print(f"Note: '{task}' is a low priority task.", end = "" )
         if time_bound == "yes":
-            message += " that requires immediate attention today!"
+            print(" that requires immediate attention today!")
         else:
-            message += ". Consider completing it when you have free time."
+            print(" Consider completing it when you have free time.")
 
     case _:
-        message = f"Note: '{task}' has an unknown priority."
+        print(f"Note: '{task}' has an unknown priority.")
        
-
-# # Append time-sensitivity message
-# if time_bound == "yes":
-#     message += " that requires immediate attention today!"
-# else:
-#     message += ". Consider completing it when you have free time."
-
-# Output final reminder
-print(message)

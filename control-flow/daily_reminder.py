2e5016d8c99446ec4aee2e2d2f680aa04b445114
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-06-01 17:07:23 +0300

    follow alx template

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -9,18 +9,36 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 match priority:
     case "high":
         message = f"Reminder: '{task}' is a high priority task."
+        
+        if time_bound == "yes":
+            message += " that requires immediate attention today!"
+        else:
+            message += ". Consider completing it when you have free time."
+
+
     case "medium":
         message = f"Reminder: '{task}' is a medium priority task."
+        if time_bound == "yes":
+            message += " that requires immediate attention today!"
+        else:
+            message += ". Consider completing it when you have free time."
+
     case "low":
         message = f"Note: '{task}' is a low priority task."
+        if time_bound == "yes":
+            message += " that requires immediate attention today!"
+        else:
+            message += ". Consider completing it when you have free time."
+
     case _:
         message = f"Note: '{task}' has an unknown priority."
+       
 
-# Append time-sensitivity message
-if time_bound == "yes":
-    message += " that requires immediate attention today!"
-else:
-    message += ". Consider completing it when you have free time."
+# # Append time-sensitivity message
+# if time_bound == "yes":
+#     message += " that requires immediate attention today!"
+# else:
+#     message += ". Consider completing it when you have free time."
 
 # Output final reminder
 print(message)
